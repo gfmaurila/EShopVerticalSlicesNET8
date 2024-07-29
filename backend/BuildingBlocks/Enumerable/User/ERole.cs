cf1722af7420a801e9dfa2a757ce6912616b3776
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2024-07-29 19:47:21 -0300

    -

---
+++ backend/BuildingBlocks/Enumerable/User/ERole.cs
@@ -0,0 +1,31 @@
+﻿namespace Enumerable.User;
+
+public enum ERole
+{
+    AdminCommand = 50601,
+    AdminQuery = 50602,
+
+    RegisterCommand = 50681,
+    RegisterQuery = 50682,
+
+    BasketCommand = 50621,
+    BasketQuery = 50622,
+
+    CatalogCommand = 50631,
+    CatalogQuery = 50632,
+
+    EmployeeCommand = 50641,
+    EmployeeQuery = 50642,
+
+    IdentityCommand = 50651,
+    IdentityQuery = 50652,
+
+    OrderingCommand = 50661,
+    OrderingQuery = 50662,
+
+    PaymentCommand = 50671,
+    PaymentQuery = 50672,
+
+    WebhooksCommand = 50691,
+    WebhooksQuery = 50692,
+}
